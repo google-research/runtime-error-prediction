a9e84b08db5de9768109824969ad1dc10ab30ffe
David Bieber <david810@gmail.com>
2021-07-09 14:52:18 -0400

    Include generating tokenizer in test

--- core/data/test_process.py
+++ core/data/test_process.py
@@ -3,9 +3,13 @@
 import unittest
 
 from core.data import process
+from core.data import tokenize
 
 
 class ProcessTest(unittest.TestCase):
+
+  def setUp(self):
+    tokenize.generate_tokenizer()
 
   def test_make_runtimeerrorproblem(self):
     source = """n = input()
@@ -13,9 +17,9 @@ print(any(set('47') >= set(str(i)) and n % i == 0 for i in range(1, n+1)) and 'Y
 """
     problem = process.make_runtimeerrorproblem(source, '1')
     target_tokens = [
-        67, 31, 100, 85, 126, 10, 382, 10, 212, 222, 322, 221, 236, 212, 10,
-        167, 10, 62, 96, 149, 67, 8, 62, 80, 18, 90, 62, 81, 105, 10, 19, 14,
-        67, 13, 19, 96, 149, 9, 240, 9, 83, 9, 238, 221]
+        66, 31, 99, 84, 125, 10, 377, 10, 214, 223, 315, 222, 237, 214, 10,
+        166, 10, 61, 95, 148, 66, 8, 61, 79, 18, 89, 61, 80, 104, 10, 19, 14,
+        66, 13, 19, 95, 148, 9, 240, 9, 82, 9, 239, 222]
     self.assertEqual(problem.tokens, target_tokens)
 
 
