53860a2e8051b2d4ac0219c15c9d6537eb8ca6aa
David Bieber <david810@gmail.com>
2021-07-09 13:27:13 -0400

    Finish and include example of parsing and tokenizing Python3 source into tokens with associated token spans.

--- src/data/process.py
+++ src/data/process.py
@@ -6,6 +6,7 @@ from typing import List, Text
 
 import dataclasses
 from python_graphs import control_flow
+from src.data import tokenize
 
 
 @dataclasses.dataclass
@@ -22,11 +23,11 @@ class RuntimeErrorProblem:
 def get_character_index(source, lineno, col_offset):
   lines = source.split('\n')
   line_index = lineno - 1
-  line_start = sum(len(line) for line in lines[:line_index])
+  line_start = sum(len(line) + 1 for line in lines[:line_index])
   return line_start + col_offset
 
 
-def process(source, target):
+def make_runtimeerrorproblem(source, target):
   """Constructs a RuntimeErrorProblem from the provided source and target.
 
   TODO(dbieber): Use target in RuntimeErrorProblem.
@@ -76,6 +77,44 @@ def process(source, target):
       node_span_starts=node_span_starts,
       node_span_ends=node_span_ends,
   )
+
+
+def tokenize_example_with_spans(tokenizer, example):
+  return tokenize_with_spans(tokenizer, example.source, example.node_span_starts, example.node_span_ends)
+
+
+def tokenize_with_spans(tokenizer, source, node_span_starts, node_span_ends):
+  tokenized = tokenizer(source, return_offsets_mapping=True)
+  tokens = tokenized['input_ids']
+  offset_mapping = tokenized['offset_mapping']
+  token_starts, token_ends = zip(*offset_mapping)
+
+  node_token_span_starts = []
+  node_token_span_ends = []
+  for i, (node_span_start, node_span_end) in enumerate(zip(node_span_starts, node_span_ends)):
+    # Want first token starting before or at node_span_start
+    node_token_span_start = token_starts.index(node_span_start)
+    # Want first token starting after or at node_span_end
+    node_token_span_end = token_ends.index(node_span_end)
+    node_token_span_starts.append(node_token_span_start)
+    node_token_span_ends.append(node_token_span_end)
+
+  return {
+      'tokens': tokens,
+      'node_token_span_starts': node_token_span_starts,
+      'node_token_span_ends': node_token_span_ends,
+  }
+
+
+def demo_parse_code():
+  """Demonstration of making a processing a RuntimeErrorProblem."""
+  source = """n = input()
+print(any(set('47') >= set(str(i)) and n % i == 0 for i in range(1, n+1)) and 'YES' or 'NO')
+"""
+  example = make_runtimeerrorproblem(source, '1')
+  tokenizer = tokenize.load_tokenizer()
+  data = tokenize_example_with_spans(tokenizer, example)
+
 
 if __name__ == '__main__':
   fire.Fire()
