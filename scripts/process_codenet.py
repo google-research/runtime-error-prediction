8ddc726adf78d13691450ec1e8c231cd7c05b28f
David Bieber <david810@gmail.com>
2021-07-16 14:57:31 -0400

    read all files

--- scripts/process_codenet.py
+++ scripts/process_codenet.py
@@ -8,6 +8,9 @@ def process_codenet():
   for problem_id in codenet.get_all_problem_ids():
     for submission_id in codenet.get_all_submission_ids(problem_id):
       python_path = codenet.get_python_path(problem_id, submission_id)
+      with open(python_path, 'r') as f:
+        source = f.read()
+
       count += 1
       if count % 1000 == 0:
         print(count)
