fe32ac9f5820cfb5d105c13f835f47da1d189426
David Bieber <david810@gmail.com>
2021-07-16 14:54:23 -0400

    Count files

--- scripts/process_codenet.py
+++ scripts/process_codenet.py
@@ -4,7 +4,13 @@ from core.data import process
 import fire
 
 def process_codenet():
-  print('TODO: process_codenet')
+  count = 0
+  for problem_id in codenet.get_all_problem_ids():
+    for submission_id in codenet.get_all_submission_ids(problem_id):
+      python_path = codenet.get_python_path(problem_id, submission_id)
+      count += 1
+      if count % 1000 == 0:
+        print(count)
 
 
 if __name__ == '__main__':
