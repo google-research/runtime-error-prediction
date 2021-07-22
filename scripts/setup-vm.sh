d72b4cc90a4c3b229e3c67b92ea2a2a8d99f116e
David Bieber <david810@gmail.com>
2021-07-22 09:22:45 -0400

    Update requirements

--- scripts/setup-vm.sh
+++ scripts/setup-vm.sh
@@ -1,15 +1,15 @@
 sudo apt-get update
 
+sudo apt install software-properties-common
+sudo apt install git zip emacs
 sudo apt-get install make build-essential libssl-dev zlib1g-dev \
 libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
 libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
 
+sudo apt install gcc
 
-sudo apt update
 sudo apt install python3 python3-dev python3-venv
-
-
-sudo apt install gcc make
+sudo apt install graphviz-dev
 
 wget https://bootstrap.pypa.io/get-pip.py
 sudo python3 get-pip.py
