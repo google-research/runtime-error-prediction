ab71f99444ce051d3b636685268e56bab80f438b
David Bieber <david810@gmail.com>
2021-07-22 10:09:45 -0400

    decode_fn

--- core/data/data_io.py
+++ core/data/data_io.py
@@ -17,3 +17,18 @@ def to_tf_example(problem):
       "node_token_span_ends": _float_feature(problem.node_token_span_ends),
       "target": _float_feature([problem.target]),
   }))
+
+
+def decode_fn(record_bytes):
+  return tf.io.parse_single_example(
+      record_bytes,
+      {
+          "tokens": tf.io.FixedLenFeature([], dtype=tf.float32),
+          "edge_sources": tf.io.FixedLenFeature([], dtype=tf.float32),
+          "edge_dests": tf.io.FixedLenFeature([], dtype=tf.float32),
+          "edge_types": tf.io.FixedLenFeature([], dtype=tf.float32),
+          "node_token_span_starts": tf.io.FixedLenFeature([], dtype=tf.float32),
+          "node_token_span_ends": tf.io.FixedLenFeature([], dtype=tf.float32),
+          "target": tf.io.FixedLenFeature([], dtype=tf.float32),
+      }
+  )
