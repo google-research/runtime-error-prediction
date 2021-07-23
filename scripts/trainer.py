ff2db029f95bcfe1c4e9befc9c38d1b446d4a505
David Bieber <david810@gmail.com>
2021-07-23 11:32:28 -0400

    batch dim

--- scripts/trainer.py
+++ scripts/trainer.py
@@ -49,12 +49,22 @@ class MlpModel(nn.Module):
   @nn.compact
   def __call__(self, x):
     x = x['tokens']
-    x = nn.Embed(num_embeddings=30000, features=128)(x)
-    x = nn.Dense(features=30)(x[:30])
+    print('x.shape')
+    print(x.shape)
+    # x.shape: batch_size, length
+    batch_size = x.shape[0]
+    x = nn.Embed(num_embeddings=30000, features=128)(x[:, :30])
+    # x.shape: batch_size, 30, 128
+    x = nn.Dense(features=30)(x)
+    # x.shape: batch_size, 30, 30
+    x = jnp.reshape(x, (batch_size, -1))
+    # x.shape: batch_size, 900
     x = nn.relu(x)
     x = nn.Dense(features=30)(x)
+    # x.shape: batch_size, 30
     x = nn.relu(x)
     x = nn.Dense(features=NUM_CLASSES)(x)
+    # x.shape: batch_size, NUM_CLASSES
     return x
 
 
@@ -80,9 +90,7 @@ def run_train(dataset_path=DEFAULT_DATASET_PATH):
   for batch in tfds.as_numpy(dataset):
     state, aux = train_step(state, batch)
     print(aux)
-    print(aux['logits'].shape)
-
-  return state
+    print(aux['logits'].shape)  # 8, 801, 29
 
 
 if __name__ == '__main__':
