c8f142290f62f815117ac5e08cd2db3d0eff304b
RishabGoel <rgoel0112@gmail.com>
2021-08-02 15:54:14 -0400

    addressed PR comments

--- config/default.py
+++ config/default.py
@@ -17,83 +17,79 @@
 
 import ml_collections
 
-from core.data import data_io
-from core.data import codenet_paths
+from core.data import codenet_paths, data_io
 
 DEFAULT_DATASET_PATH = codenet_paths.DEFAULT_DATASET_PATH
 Config = ml_collections.ConfigDict
 
 
 def default_config():
-  """Gets the default config for the interpreter model."""
-  config = Config()
-  config.overrides = ''
-  config.seed_id = 0
-  config.debug = False
+    """Gets the default config for the interpreter model."""
+    config = Config()
+    config.overrides = ""
+    config.seed_id = 0
+    config.debug = False
 
-  config.setup = Config()
-  config.setup.setup_dataset = True
-  config.setup.setup_model = True
+    config.setup = Config()
+    config.setup.setup_dataset = True
+    config.setup.setup_model = True
 
-  config.logging = Config()
-  config.logging.summary_freq = 500
-  config.logging.save_freq = 2
+    config.logging = Config()
+    config.logging.summary_freq = 500
+    config.logging.save_freq = 2
 
-  config.runner = Config()
-  config.runner.mode = 'train'
-  config.runner.method = 'supervised'
-  config.runner.experiment_kind = 'train + eval'
-  config.runner.restart_behavior = 'restore'  # abort or restore
-  config.runner.early_stopping_threshold = 1
-  config.runner.early_stopping_delta = 0.0
-  config.runner.epochs = 8
+    config.runner = Config()
+    config.runner.mode = "train"
+    config.runner.method = "supervised"
+    config.runner.experiment_kind = "train + eval"
+    config.runner.restart_behavior = "restore"  # abort or restore
+    config.runner.early_stopping_threshold = 1
+    config.runner.early_stopping_delta = 0.0
+    config.runner.epochs = 8
 
-  config.checkpoint = Config()
-  config.checkpoint.run_dir = ''
-  config.checkpoint.path = 'checkpoints/'
-  config.checkpoint.id = 0
+    config.checkpoint = Config()
+    config.checkpoint.run_dir = ""
+    config.checkpoint.path = "checkpoints/"
+    config.checkpoint.id = 0
 
-  config.dataset = Config()
-  config.dataset.name = 'handcrafted-10'
-  config.dataset.train_path = DEFAULT_DATASET_PATH
-  config.dataset.eval_path = DEFAULT_DATASET_PATH
-  config.dataset.version = 'default'  # Set to use an explicit dataset version.
-  config.dataset.split = 'default'
-  config.dataset.representation = 'code'  # code, trace
-  config.dataset.max_length = 10000
-  config.dataset.max_tokens = 71
-  config.dataset.batch_size = 8
-  config.dataset.vocab_size = 20000
-  config.dataset.batch = True
-  config.dataset.in_memory = False
+    config.dataset = Config()
+    config.dataset.name = "handcrafted-10"
+    config.dataset.train_path = DEFAULT_DATASET_PATH
+    config.dataset.eval_path = DEFAULT_DATASET_PATH
+    config.dataset.version = "default"  # Set to use an explicit dataset version.
+    config.dataset.split = "default"
+    config.dataset.representation = "code"  # code, trace
+    config.dataset.max_length = 10000
+    config.dataset.max_tokens = 71
+    config.dataset.batch_size = 8
+    config.dataset.vocab_size = 20000
+    config.dataset.batch = True
+    config.dataset.in_memory = False
 
-  config.train = Config()
-  config.train.total_steps = 0  # 0 means no limit.
+    config.train = Config()
+    config.train.total_steps = 0  # 0 means no limit.
 
-  config.opt = Config()
-  config.opt.learning_rate = 0.0003
+    config.opt = Config()
+    config.opt.learning_rate = 0.0003
 
+    # Model configs.
+    config.model = Config()
+    config.model.name = "StackedLSTMModel"
+    config.model.hidden_size = 64
 
-  # Model configs.
-  config.model = Config()
-  config.model.name = 'StackedLSTMModel'
-  config.model.hidden_size = 64
+    # Other configs.
+    config.eval_name = ""
+    config.eval_steps = 2
+    # Number of seconds to wait without receiving checkpoint before timing out.
+    config.eval_timeout = 30 * 60  # 30 minutes.
+    config.eval_metric = "F1-score"
 
-
-  # Other configs.
-  config.eval_name = ''
-  config.eval_steps = 2
-  # Number of seconds to wait without receiving checkpoint before timing out.
-  config.eval_timeout = 30 * 60  # 30 minutes.
-  config.eval_metric = "F1-score"
-
-
-  config.index = 0
-  return config
+    config.index = 0
+    return config
 
 
 def get_config():
-  """Gets the config for the interpreter model."""
-  config = default_config()
-  config.lock()
-  return config
+    """Gets the config for the interpreter model."""
+    config = default_config()
+    config.lock()
+    return config
