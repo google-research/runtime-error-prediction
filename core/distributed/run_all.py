a61c5618dc9399b91507df06ff78d4ab9fcf5494
David Bieber <david810@gmail.com>
2021-08-17 15:16:50 -0400

    run_all script

---
+++ core/distributed/run_all.py
@@ -0,0 +1,45 @@
+import getpass
+
+from core.distributed import gcp
+
+wait = gcp.wait
+parallel = gcp.parallel
+
+
+access_token = getpass.getpass('Personal access token: ')
+repo_url = f'https://{access_token}@github.com/googleprivate/compressive-ipagnn.git'
+
+
+def clone_args(index):
+  branch = '2021-08-13-overfit'
+  hostname = _hostname(index)
+  zone = _zone(index)
+  return ['gcloud', 'compute', 'ssh', hostname, '--command',
+          f'git clone {repo_url} && cd compressive-ipagnn && git checkout {branch}',
+          '--zone', zone]
+
+
+def clone(index):
+  args = clone_args(index)
+  return call(args)
+
+
+def setup_args(index):
+  hostname = _hostname(index)
+  zone = _zone(index)
+  return ['gcloud', 'compute', 'ssh', hostname, '--command',
+          f'compressive-ipagnn/core/distributed/setup.sh',
+          '--zone', zone]
+
+
+def setup(index):
+  args = setup_args(index)
+  return call(args)
+
+
+n = 2
+gcp.up_n(n)
+
+wait(parallel(clone, n=n))
+# wait(parallel(setup, n=n))
+gcp.down_n(n)
