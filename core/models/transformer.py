cf4486377f731ea634ebd2a910e9ca9ba150158c
David Bieber <david810@gmail.com>
2021-08-24 17:56:06 -0400

    Move models to models dir

---
+++ core/models/transformer.py
@@ -0,0 +1,62 @@
+from typing import Any
+
+from flax import linen as nn
+import jax.numpy as jnp
+
+from core.data import error_kinds
+from core.modules.ipagnn import encoder
+from core.modules.ipagnn import ipagnn
+from core.modules.ipagnn import spans
+from third_party.flax_examples import transformer_modules
+
+NUM_CLASSES = error_kinds.NUM_CLASSES
+
+
+class Transformer(nn.Module):
+
+  config: Any
+
+  def setup(self):
+    config = self.config
+    vocab_size = config.vocab_size  # TODO(dbieber): Load from tokenizer / info.
+    max_tokens = config.max_tokens
+    max_num_nodes = config.max_num_nodes
+    max_num_edges = config.max_num_edges
+    info = ipagnn.Info(vocab_size=vocab_size)
+    transformer_config = transformer_modules.TransformerConfig(
+        vocab_size=vocab_size,
+        output_vocab_size=vocab_size,
+    )
+    self.token_embedder = spans.NodeAwareTokenEmbedder(
+        transformer_config=transformer_config,
+        num_embeddings=vocab_size,
+        features=config.model.hidden_size,
+        max_tokens=max_tokens,
+        max_num_nodes=max_num_nodes,
+    )
+    self.encoder = encoder.TransformerEncoder(transformer_config)
+
+  @nn.compact
+  def __call__(self, x):
+    tokens = x['tokens']
+    # tokens.shape: batch_size, max_tokens
+    tokens_mask = tokens > 0
+    # tokens_mask.shape: batch_size, max_tokens
+    encoder_mask = nn.make_attention_mask(tokens_mask, tokens_mask, dtype=jnp.float32)
+    # encoder_mask.shape: batch_size, 1, max_tokens, max_tokens
+    encoded_inputs = self.token_embedder(
+        tokens, x['node_token_span_starts'], x['node_token_span_ends'])
+    # encoded_inputs.shape: batch_size, max_tokens, hidden_size
+    encoding = self.encoder(encoded_inputs, encoder_mask=encoder_mask)
+    # encoding.shape: batch_size, max_tokens, hidden_size
+
+    # Mean pooling.
+    tokens_mask_ext = tokens_mask[:, :, None]
+    x = (
+        jnp.sum(encoding * tokens_mask_ext, axis=1)
+        / jnp.maximum(1, jnp.sum(tokens_mask_ext, axis=1))
+    )
+    # x.shape: batch_size, hidden_size
+    x = nn.Dense(features=NUM_CLASSES)(x)
+    # x.shape: batch_size, NUM_CLASSES
+    return x
--- scripts/trainer.py
+++ scripts/trainer.py
@@ -26,9 +26,9 @@ from core.data import codenet_paths
 from core.data import data_io
 from core.data import error_kinds
 from core.lib import optimizer_lib
-from core.modules.ipagnn import encoder
-from core.modules.ipagnn import ipagnn
-from core.modules.ipagnn import spans
+from core.models import ipagnn
+from core.models import mlp
+from core.models import transformer
 from third_party.flax_examples import transformer_modules
 
 
@@ -36,139 +36,6 @@ DEFAULT_DATASET_PATH = codenet_paths.DEFAULT_DATASET_PATH
 NUM_CLASSES = error_kinds.NUM_CLASSES
 
 Config = ml_collections.ConfigDict
-
-
-class MlpModel(nn.Module):
-
-  @nn.compact
-  def __call__(self, x):
-    x = x['tokens']
-    # x.shape: batch_size, length
-    batch_size = x.shape[0]
-    x = nn.Embed(num_embeddings=30000, features=128)(x[:, :30])
-    # x.shape: batch_size, 30, 128
-    x = nn.Dense(features=30)(x)
-    # x.shape: batch_size, 30, 30
-    x = jnp.reshape(x, (batch_size, -1))
-    # x.shape: batch_size, 900
-    x = nn.relu(x)
-    x = nn.Dense(features=30)(x)
-    # x.shape: batch_size, 30
-    x = nn.relu(x)
-    x = nn.Dense(features=NUM_CLASSES)(x)
-    # x.shape: batch_size, NUM_CLASSES
-    return x
-
-
-class Transformer(nn.Module):
-
-  config: Any
-
-  def setup(self):
-    config = self.config
-    vocab_size = config.vocab_size  # TODO(dbieber): Load from tokenizer / info.
-    max_tokens = config.max_tokens
-    max_num_nodes = config.max_num_nodes
-    max_num_edges = config.max_num_edges
-    info = ipagnn.Info(vocab_size=vocab_size)
-    transformer_config = transformer_modules.TransformerConfig(
-        vocab_size=vocab_size,
-        output_vocab_size=vocab_size,
-    )
-    self.token_embedder = spans.NodeAwareTokenEmbedder(
-        transformer_config=transformer_config,
-        num_embeddings=vocab_size,
-        features=config.model.hidden_size,
-        max_tokens=max_tokens,
-        max_num_nodes=max_num_nodes,
-    )
-    self.encoder = encoder.TransformerEncoder(transformer_config)
-
-  @nn.compact
-  def __call__(self, x):
-    tokens = x['tokens']
-    # tokens.shape: batch_size, max_tokens
-    tokens_mask = tokens > 0
-    # tokens_mask.shape: batch_size, max_tokens
-    encoder_mask = nn.make_attention_mask(tokens_mask, tokens_mask, dtype=jnp.float32)
-    # encoder_mask.shape: batch_size, 1, max_tokens, max_tokens
-    encoded_inputs = self.token_embedder(
-        tokens, x['node_token_span_starts'], x['node_token_span_ends'])
-    # encoded_inputs.shape: batch_size, max_tokens, hidden_size
-    encoding = self.encoder(encoded_inputs, encoder_mask=encoder_mask)
-    # encoding.shape: batch_size, max_tokens, hidden_size
-
-    # Mean pooling.
-    tokens_mask_ext = tokens_mask[:, :, None]
-    x = (
-        jnp.sum(encoding * tokens_mask_ext, axis=1)
-        / jnp.maximum(1, jnp.sum(tokens_mask_ext, axis=1))
-    )
-    # x.shape: batch_size, hidden_size
-    x = nn.Dense(features=NUM_CLASSES)(x)
-    # x.shape: batch_size, NUM_CLASSES
-    return x
-
-
-class IPAGNN(nn.Module):
-
-  config: Any
-
-  def setup(self):
-    config = self.config
-    vocab_size = config.vocab_size  # TODO(dbieber): Load from tokenizer / info.
-    max_tokens = config.max_tokens
-    max_num_nodes = config.max_num_nodes
-    max_num_edges = config.max_num_edges
-    max_steps = config.max_steps
-    info = ipagnn.Info(vocab_size=vocab_size)
-    # TODO(dbieber): transformer_config is unused.
-    transformer_config = transformer_modules.TransformerConfig(
-        vocab_size=vocab_size,
-        output_vocab_size=vocab_size,
-    )
-    self.node_span_encoder = spans.NodeSpanEncoder(
-        info=info,
-        config=config,
-        max_tokens=max_tokens,
-        max_num_nodes=max_num_nodes,
-    )
-
-    self.ipagnn = ipagnn.IPAGNNModule(
-        info=info,
-        config=config,
-        max_steps=max_steps,
-    )
-
-  @nn.compact
-  def __call__(self, x):
-    tokens = x['tokens']
-    # tokens.shape: batch_size, max_tokens
-    batch_size = tokens.shape[0]
-    encoded_inputs = self.node_span_encoder(
-        tokens, x['node_token_span_starts'], x['node_token_span_ends'])
-    # encoded_inputs.shape: batch_size, max_num_nodes, hidden_size
-    ipagnn_output = self.ipagnn(
-        node_embeddings=encoded_inputs,
-        edge_sources=x['edge_sources'],
-        edge_dests=x['edge_dests'],
-        edge_types=x['edge_types'],
-        true_indexes=x['true_branch_nodes'],
-        false_indexes=x['false_branch_nodes'],
-        exit_indexes=x['exit_index'],
-        step_limits=x['step_limit'],
-    )
-    # ipagnn_output['node_embeddings'].shape: batch_size, max_num_nodes, hidden_size
-    # ipagnn_output['instruction_pointer'].shape: batch_size, max_num_nodes
-    # ipagnn_output['exit_node_embeddings'].shape: batch_size, hidden_size
-    # ipagnn_output['exception_node_embeddings'].shape: batch_size, hidden_size
-
-    # TODO(dbieber): Reevaluate how to go from transformer encodings to output.
-    exit_node_embeddings = ipagnn_output['exit_node_embeddings']
-    # exit_node_embeddings.shape: batch_size, emb_dim
-    logits = nn.Dense(features=NUM_CLASSES)(exit_node_embeddings)
-    # logits.shape: batch_size, NUM_CLASSES
-    return logits
 
 
 class TrainState(train_state.TrainState):
@@ -242,11 +109,11 @@ class Trainer:
     config = self.make_sample_config()
     model_class = self.model_class
     if model_class == 'MlpModel':
-      return MlpModel()
+      return mlp.MlpModel()
     elif model_class == 'Transformer':
-      return Transformer(config=config)
+      return transformer.Transformer(config=config)
     elif model_class == 'IPAGNN':
-      return IPAGNN(config=config)
+      return ipagnn.IPAGNN(config=config)
     else:
       raise ValueError('Unexpected model_class.')
 
