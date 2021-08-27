2a0a79b0bb0e11a6595295ee3fc59aedbc88cd44
David Bieber <david810@gmail.com>
2021-08-27 11:31:17 -0400

    Additional tests

--- core/modules/ipagnn/test_logit_math.py
+++ core/modules/ipagnn/test_logit_math.py
@@ -1,5 +1,6 @@
 """Tests for logit_math.py."""
 
+import jax
 import jax.numpy as jnp
 import unittest
 
@@ -13,26 +14,42 @@ class LogitMathTest(unittest.TestCase):
     target_p = 0.2
     additional_logit = logit_math.get_additional_logit(target_p, logits)
     self.assertEqual(additional_logit, 2)
+    combined_logits = jnp.concatenate([logits, jnp.array([additional_logit])])
+    self.assertEqual(jax.nn.softmax(combined_logits)[-1], target_p)
 
     logits = jnp.array([-jnp.inf, 3, 3, 3])
     target_p = 0.25
     additional_logit = logit_math.get_additional_logit(target_p, logits)
     self.assertEqual(additional_logit, 3)
+    combined_logits = jnp.concatenate([logits, jnp.array([additional_logit])])
+    self.assertEqual(jax.nn.softmax(combined_logits)[-1], target_p)
 
     logits = jnp.array([-jnp.inf, 1, 2, 3])
     target_p = 1
     additional_logit = logit_math.get_additional_logit(target_p, logits)
     self.assertEqual(additional_logit, jnp.inf)
+    combined_logits = jnp.concatenate([logits, jnp.array([additional_logit])])
+    # softmax(combined_logits) includes nan.
+
+    logits = jnp.array([-jnp.inf, 1, 2, 3])
+    target_p = .9999
+    additional_logit = logit_math.get_additional_logit(target_p, logits)
+    combined_logits = jnp.concatenate([logits, jnp.array([additional_logit])])
+    self.assertEqual(jax.nn.softmax(combined_logits)[-1], target_p)
 
     logits = jnp.array([-jnp.inf, -1, -2, -3])
     target_p = 0
     additional_logit = logit_math.get_additional_logit(target_p, logits)
     self.assertEqual(additional_logit, -jnp.inf)
+    combined_logits = jnp.concatenate([logits, jnp.array([additional_logit])])
+    self.assertEqual(jax.nn.softmax(combined_logits)[-1], target_p)
 
     logits = jnp.array([-jnp.inf, -jnp.inf, jnp.log(2), jnp.log(3), jnp.log(5)])
     target_p = 2/3
     additional_logit = logit_math.get_additional_logit(target_p, logits)
     self.assertEqual(additional_logit, jnp.log(20))
+    combined_logits = jnp.concatenate([logits, jnp.array([additional_logit])])
+    self.assertEqual(jax.nn.softmax(combined_logits)[-1], target_p)
 
 
 if __name__ == '__main__':
