2ed498382867806fa1e8d1caae5ccf4b70ff9948
David Bieber <david810@gmail.com>
2021-08-30 15:46:50 -0400

    print more

--- core/data/explore.py
+++ core/data/explore.py
@@ -9,7 +9,7 @@ def get_spans(problem_id, submission_id, tokenizer_path=None):
   problem = make_runtimeerrorproblem(
       source, target, tokenizer=tokenizer, problem_id=problem_id, submission_id=submission_id)
 
-
+  print(source)
   tokens = tokenizer.convert_ids_to_tokens(problem.tokens)
-  for span_index, span_start, span_end in enumerate(zip(problem.node_token_span_starts, problem.node_token_span_ends)):
+  for span_index, (span_start, span_end) in enumerate(zip(problem.node_token_span_starts, problem.node_token_span_ends)):
     print(f'Span {span_index}: {tokens[span_start:span_end + 1]}')
