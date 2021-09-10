70f86b21d93c23ebb47197b70e9908147f904d10
David Bieber <david810@gmail.com>
2021-09-10 13:53:04 -0400

    Sweep lr

--- core/distributed/sweep.py
+++ core/distributed/sweep.py
@@ -6,7 +6,11 @@ from core.distributed import gcp
 
 
 hparams = {
-    'config.learning_rate': [0.001, 0.003, 0.01, 0.03, 0.1, 0.3],
+    'config.learning_rate': [
+        1e-5, 3e-5,
+        1e-4, 3e-4, 0.001, 0.003,
+        # 0.01, 0.03, 0.1, 0.3,
+    ],
     # 'config.rnn_layers': [2, 4]
     'config.grad_clip_value': [0, 0.5, 1, 2],
     'config.hidden_size': [16, 32, 64, 128, 256, 512],
