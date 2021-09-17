3345894ffc8241f356fdbf81fb4dc01f768375ae
David Bieber <david810@gmail.com>
2021-09-17 13:42:31 -0400

    Set ids as strings

--- scripts/process_cfp.py
+++ scripts/process_cfp.py
@@ -61,14 +61,13 @@ def process_control_flow_programs(
     if count < start_at:
       continue
 
-    python_path = f'{tfrecord_pattern}:{index}'
     source = example['human_readable_code'][0].numpy().decode('utf-8')
     target = example['target_output'][0].numpy()
     original_step_limit = example['cfg_forward/steps'][0].numpy()
 
     problem = process.make_runtimeerrorproblem(
         source, target, tokenizer=tokenizer,
-        problem_id=index, submission_id=0)
+        problem_id=tfrecord_pattern, submission_id=str(index))
     assert problem.step_limit == original_step_limit
     yield problem
 
