879cec234e4887f704d3937f51ff7efc8218177e
David Bieber <david810@gmail.com>
2021-10-05 13:15:51 -0400

    Use new get_branches api

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DEPENDENCIES = [
     'jaxlib>=0.1.69',
     'matplotlib',
     'ml_collections',
-    'python-graphs>=1.1.0',
+    'python-graphs>=1.2.0',
     'sklearn',
     'tensorflow',
     'tensorflow_datasets',
