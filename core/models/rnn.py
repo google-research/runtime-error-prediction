fd2b3c5a2d8cb27930053dfcce15b926cf88f5c6
RishabGoel <rgoel0112@gmail.com>
2021-10-18 20:30:43 -0400

    added the rnn baseline

---
+++ core/models/rnn.py
@@ -0,0 +1,67 @@
+from typing import Any
+
+import jax
+from flax import linen as nn
+import jax.numpy as jnp
+
+from core.modules.ipagnn import encoder
+from core.modules.ipagnn import spans
+from third_party.flax_examples import transformer_modules, lstm_modules
+
+
+class LSTM(nn.Module):
+
+  config: Any
+  info: Any
+  transformer_config: transformer_modules.TransformerConfig
+
+  def setup(self):
+    config = self.config
+    vocab_size = self.info.vocab_size
+    max_tokens = config.max_tokens
+    max_num_nodes = config.max_num_nodes
+    max_num_edges = config.max_num_edges
+    lstm_config = lstm_modules.LSTMConfig(
+      vocab_size=vocab_size, 
+      num_layers=config.rnn_layers, 
+      hidden_dim=config.hidden_size,)
+    self.token_embedder = spans.NodeAwareTokenEmbedder(
+        transformer_config=self.transformer_config,
+        num_embeddings=vocab_size,
+        features=config.hidden_size,
+        max_tokens=max_tokens,
+        max_num_nodes=max_num_nodes,
+    )
+    self.encoder = encoder.LSTMEncoder(lstm_config)
+
+
+  @nn.compact
+  def __call__(self, x):
+    tokens = x['tokens']
+    # tokens.shape: batch_size, max_tokens
+    tokens_mask = tokens > 0
+    # tokens_mask.shape: batch_size, max_tokens
+    encoder_mask = nn.make_attention_mask(tokens_mask, tokens_mask, dtype=jnp.float32)
+    # encoder_mask.shape: batch_size, 1, max_tokens, max_tokens
+    # NOTE(rgoel): Ensuring the token encoder is still a Transformer to ensure uniformity.
+    encoded_inputs = self.token_embedder(
+        tokens, x['node_token_span_starts'], x['node_token_span_ends'],
+        x['num_nodes'])
+    # encoded_inputs.shape: batch_size, max_tokens, hidden_size
+    encoded_inputs = self.encoder(encoded_inputs)
+    # encoded_inputs.shape: batch_size, max_tokens, hidden_size
+
+    # NOTE(rgoel): Using only the last state. We can change this to
+    # pooling across time stamps.
+    def get_last_state(inputs, last_token):
+      return inputs[last_token-1]
+
+    get_last_state_batch = jax.vmap(get_last_state)
+    # encoded_inputs.shape: batch_size, max_tokens, hidden_size
+    x = get_last_state_batch(encoded_inputs, x['num_tokens'])
+    # x.shape: batch_size, 1, hidden_size
+    x = jnp.squeeze(x, 1)
+    # x.shape: batch_size, hidden_size
+    x = nn.Dense(features=self.info.num_classes)(x)
+    # x.shape: batch_size, num_classes
+    return x, None
--- core/modules/ipagnn/encoder.py
+++ core/modules/ipagnn/encoder.py
@@ -1,6 +1,6 @@
 from flax import linen as nn
 
-from third_party.flax_examples import transformer_modules
+from third_party.flax_examples import transformer_modules, lstm_modules
 
 Encoder1DBlock = transformer_modules.Encoder1DBlock
 
@@ -39,6 +39,45 @@ class TransformerEncoder(nn.Module):
     # Input Encoder
     for lyr in range(cfg.num_layers):
       x = Encoder1DBlock(config=cfg, name=f'encoderblock_{lyr}')(x, encoder_mask)
+
+    encoded = nn.LayerNorm(dtype=cfg.dtype, name='encoder_norm')(x)
+
+    return encoded
+
+
+class LSTMEncoder(nn.Module):
+  """LSTM Model Encoder for sequence to sequence translation.
+
+  This Encoder does not encode the input
+  tokens itself. It assumes the tokens have already been encoded, and any
+  desired positional embeddings have already been aded.
+
+  Attributes:
+    config: LSTMConfig dataclass containing hyperparameters.
+  """
+  config: lstm_modules.LSTMConfig
+
+  @nn.compact
+  def __call__(self,
+               encoded_inputs,):
+    """Applies Transformer model on the encoded inputs.
+
+    Args:
+      encoded_inputs: pre-encoded input data.
+
+    Returns:
+      output of a lstm encoder.
+    """
+    cfg = self.config
+    batch_size, max_tokens, _ = encoded_inputs.shape
+    x = encoded_inputs
+    x = nn.Dropout(rate=cfg.dropout_rate)(x, deterministic=cfg.deterministic)
+    x = x.astype(cfg.dtype)
+
+    # Input Encoder
+    for lyr in range(cfg.num_layers):
+      initial_state = lstm_modules.SimpleLSTM().initialize_carry((batch_size,), cfg.hidden_dim)
+      _, x = lstm_modules.SimpleLSTM()(initial_state, x) # TODO(rgoel): Add layer norm to each layer
 
     encoded = nn.LayerNorm(dtype=cfg.dtype, name='encoder_norm')(x)
 
