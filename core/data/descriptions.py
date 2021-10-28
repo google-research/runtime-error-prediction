8c13c46e0e2536966140e1f75172bde15b4f711c
David Bieber <david810@gmail.com>
2021-10-28 14:42:45 -0400

    support japanese

--- core/data/descriptions.py
+++ core/data/descriptions.py
@@ -4,6 +4,9 @@ import re
 
 
 HEADER_TAGS = ['h2', 'h3']
+INPUT_HEADER_NAMES = ['Input', '入力']
+CONSTRAINT_HEADER_NAMES = ['Constraints', '制約', '入力形式']
+# 入力形式 = Input format.
 
 
 def extract_input_description(text, soup=None):
@@ -18,6 +21,17 @@ def extract_input_constraints(text, soup=None):
   constraints_header = soup.find(HEADER_TAGS, text=re.compile('Constraints'))
   constraints_text = get_text_following_header(constraints_header)
   return constraints_text.strip()
+
+
+def extract_input_information(text, soup=None):
+  soup = soup or bs4.BeautifulSoup(text, 'html.parser')
+  info = []
+  for header_name in INPUT_HEADER_NAMES + CONSTRAINT_HEADER_NAMES:
+    header = soup.find(HEADER_TAGS, text=re.compile(header_name))
+    text = get_text_following_header(header)
+    if text:
+      info.append((header_name, text))
+  return '\n\n'.join(f'{header}:\n{text}')
 
 
 def get_text_following_header(header):
@@ -42,12 +56,15 @@ def get_all_input_descriptions():
     path = f'/mnt/project-codenet-storage/Project_CodeNet/problem_descriptions/p{i:05d}.html'
     with open(path, 'r') as f:
       text = f.read()
-    input_description = extract_input_description(text)
-    input_constraints = extract_input_constraints(text)
+    info = extract_input_information(text)
+    # input_description = extract_input_description(text)
+    # input_constraints = extract_input_constraints(text)
     print('Index:', i)
-    print('Description:', input_description)
-    print('Constraints:', input_constraints)
+    print(info)
     print()
+    # print('Description:', input_description)
+    # print('Constraints:', input_constraints)
+    # print()
 
 if __name__ == '__main__':
   fire.Fire()
