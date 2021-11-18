f46fb54196c059ad2f8825e403fd6c91ce5a1dc7
David Bieber <david810@gmail.com>
2021-11-18 13:05:32 -0500

    revert setup

--- scripts/setup-tpu.sh
+++ scripts/setup-tpu.sh
@@ -1,5 +1,3 @@
-echo "Starting"
-hostname
 pip install "jax[tpu]>=0.2.16" -f https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 # Clone repo
@@ -7,25 +5,20 @@ git clone https://$PERSONAL_ACCESS_TOKEN@github.com/googleprivate/compressive-ip
 
 # Install deps
 cd compressive-ipagnn
-git fetch --all
-git checkout 2021-11-09-binary-sweep
 git pull
 sudo apt-get update
-sudo apt install libgraphviz-dev tidy -y
+sudo apt install libgraphviz-dev -y
 python3 -m pip install -r requirements.txt
 
 # Connect to GCS Bucket
-# fusermount -u /mnt/runtime-error-problems-experiments
 if [ ! -f /mnt/runtime-error-problems-experiments/README.md ]; then
   sudo mkdir -p /mnt/runtime-error-problems-experiments
   sudo chown $(whoami) /mnt/runtime-error-problems-experiments
-  gcsfuse --implicit-dirs runtime-error-problems-experiments /mnt/runtime-error-problems-experiments/
+  gcsfuse runtime-error-problems-experiments /mnt/runtime-error-problems-experiments/
 fi
 
-# # Copy data out of bucket for faster access.
-# if [ ! -d project-codenet-data/full-noudf-ids ]; then
-#   mkdir -p project-codenet-data
-#   cp -r /mnt/runtime-error-problems-experiments/datasets/project-codenet/full-noudf-ids project-codenet-data/full-noudf-ids
-# fi
-hostname
-echo "Done"
+# Copy data out of bucket for faster access.
+if [ ! -d project-codenet-data/full-noudf-ids ]; then
+  mkdir -p project-codenet-data
+  cp -r /mnt/runtime-error-problems-experiments/datasets/project-codenet/full-noudf-ids project-codenet-data/full-noudf-ids
+fi
