3c08f675944008d72ff7bd9d98d532fd332ec5d8
David Bieber <david810@gmail.com>
2021-12-28 11:59:40 -0500

    Bias sweep

--- core/distributed/sweep.py
+++ core/distributed/sweep.py
@@ -298,13 +298,13 @@ def main(experiment_id=None, study_id=None, dataset_path=None, skip_create=False
   # }
   # run_sweep(n, offset, experiment_id, study_id, 'CN', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH, skip_create)
 
-  # Bias
-  # offset = 100
-  # overrides = {
-  #     'config.raise_in_ipagnn': True,
-  #     # Using config.raise_decision_offset.
-  # }
-  # run_sweep(n, offset, experiment_id, study_id, 'EO', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH, skip_create)
+  # # Bias
+  offset = 100
+  overrides = {
+      'config.raise_in_ipagnn': True,
+      # Using config.raise_decision_offset.
+  }
+  run_sweep(n, offset, experiment_id, study_id, 'EO', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH, skip_create)
 
   offset = 110
   overrides = {
