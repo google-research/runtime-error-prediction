e5732f53f73237a57d32f018abf363082d83f31b
David Bieber <david810@gmail.com>
2022-01-03 14:51:31 -0500

    IPA-GNN FiLM Concat with Docstrings sweep

--- core/distributed/sweep.py
+++ core/distributed/sweep.py
@@ -396,10 +396,28 @@ def main(experiment_id=None, study_id=None, dataset_path=None, skip_create=False
   # run_sweep(n, offset, experiment_id, study_id, 'EF', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH, skip_create, dry_run)
 
   # IPA-GNN Cross-attention
-  offset = 60
+  # offset = 60
+  # overrides = {
+  #     'config.raise_in_ipagnn': False,
+  #     'config.use_cross_attention': True,
+  #     'config.modulate_mode': 'concat',
+  # }
+  # run_sweep(n, offset, experiment_id, study_id, 'IC', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create)
+
+  # # Exception IPA-GNN Cross-attention
+  # offset = 70
+  # overrides = {
+  #     'config.raise_in_ipagnn': True,
+  #     'config.use_cross_attention': True,
+  #     'config.modulate_mode': 'concat',
+  # }
+  # run_sweep(n, offset, experiment_id, study_id, 'EC', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create)
+
+  # IPA-GNN FiLM Concat with Docstrings
+  offset = 10
   overrides = {
       'config.raise_in_ipagnn': False,
-      'config.use_cross_attention': True,
+      'config.use_film': True,
       'config.modulate_mode': 'concat',
   }
   run_sweep(n, offset, experiment_id, study_id, 'IC', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create, dry_run)
