f89b1036df7c89152c80bac387ea65e8c07c4c24
David Bieber <david810@gmail.com>
2022-01-06 17:50:57 -0500

    GGNN Sweep

--- core/distributed/sweep.py
+++ core/distributed/sweep.py
@@ -366,19 +366,6 @@ def main(experiment_id=None, study_id=None, dataset_path=None, skip_create=False
   # }
   # run_sweep(n, offset, experiment_id, study_id, 'EF', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create, dry_run)
 
-  # GGNN with docstring input
-  # offset = 70
-  # overrides = {
-  # }
-  # run_sweep(n, offset, experiment_id, study_id, 'GI', 'GGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create, dry_run)
-
-  # # GGNN with no input
-  offset = 80
-  n = 10
-  overrides = {
-  }
-  run_sweep(n, offset, experiment_id, study_id, 'GN', 'GGNN', overrides, codenet_paths.FULL_DATASET_PATH, skip_create, dry_run)
-
   # IPA-GNN FiLM
   # offset = 70
   # overrides = {
@@ -434,13 +421,26 @@ def main(experiment_id=None, study_id=None, dataset_path=None, skip_create=False
   # run_sweep(n, offset, experiment_id, study_id, 'EC', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create)
 
   # IPA-GNN FiLM Concat with Docstrings
-  offset = 10
+  # offset = 10
+  # overrides = {
+  #     'config.raise_in_ipagnn': False,
+  #     'config.use_film': True,
+  #     'config.modulate_mode': 'concat',
+  # }
+  # run_sweep(n, offset, experiment_id, study_id, 'IC', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create, dry_run)
+
+  # GGNN with docstring input
+  # offset = 70
+  # overrides = {
+  # }
+  # run_sweep(n, offset, experiment_id, study_id, 'GI', 'GGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create, dry_run)
+
+  # # GGNN with no input
+  offset = 80
+  n = 10
   overrides = {
-      'config.raise_in_ipagnn': False,
-      'config.use_film': True,
-      'config.modulate_mode': 'concat',
   }
-  run_sweep(n, offset, experiment_id, study_id, 'IC', 'IPAGNN', overrides, codenet_paths.FULL_DATASET_PATH_WITH_DOCSTRINGS, skip_create, dry_run)
+  run_sweep(n, offset, experiment_id, study_id, 'GN', 'GGNN', overrides, codenet_paths.FULL_DATASET_PATH, skip_create, dry_run)
 
 
 # # To kill the runner processes:
